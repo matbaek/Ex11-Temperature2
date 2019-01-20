01c99267d4984ab162b84bd0284b46fd61eb20cc
Mathias Bærentsen <math4249@edu.eal.dk>
2019-01-20 13:42:25 +0100

    Add project files.

---
+++ Ex11-Temperature/Temperature.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_Temperature
+{
+    public class Temperature
+    {
+        public double input;
+        public enum TemperatureScale
+        {
+            Celsius,
+            Fahrenheit
+        }
+
+
+        public double Celsius {
+            get
+            {
+                return input;
+            }
+            set
+            {
+                input = value;
+            }
+        }
+        public double Fahrenheit {
+            get
+            {
+                return ((9.0 * input) / 5.0) + 32;
+            }
+            set
+            {
+                input = 5 * (value - 32) / 9;
+            }
+        }
+
+        public void SetTemperature(double v, TemperatureScale temp)
+        {
+
+            switch (temp)
+            {
+                case TemperatureScale.Celsius:
+                    Celsius = v;
+                    break;
+                case TemperatureScale.Fahrenheit:
+                    Fahrenheit = v;
+                    break;
+            }
+        }
+    }
+}
